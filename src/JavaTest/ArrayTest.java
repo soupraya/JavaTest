ac766d21561c4698b8eb752d6122b806e7fd62dc
Velan Souprayane <soupraya@gmail.com>
2015-03-06 13:25:44 +0000

    Initial commit

---
+++ src/JavaTest/ArrayTest.java
@@ -0,0 +1,22 @@
+package JavaTest;
+
+public class ArrayTest {
+  double A;
+  double B;
+  double C;
+  double D[];
+  
+  public ArrayTest(double A, double B, double C)
+    {
+    A= this.A;
+    B= this.B;
+    C= this.C;
+	 }
+  public void addArray()
+    {
+    D[0] = A;
+    D[1] = B;
+    D[2] = C;
+  }
+  
+}
