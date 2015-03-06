d0543e1a598777701569b7bbee3f5620cad7a1e3
Velan Souprayane <soupraya@gmail.com>
2015-03-06 13:01:28 +0000

    Initial commit

--- src/JavaTest/Javatest1.java
+++ src/JavaTest/Javatest1.java
@@ -2,7 +2,7 @@ package JavaTest;
 
 public class Javatest1 {
   public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Javatest1");
     }
   
 }
